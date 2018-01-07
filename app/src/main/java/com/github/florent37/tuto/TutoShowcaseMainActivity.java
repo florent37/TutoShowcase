29ebaaec930ea11e6f8ac058623f4e5261ec523f
Ray Li <ray.li.verified@gmail.com>
2018-01-06 20:38:09 -0600

    Code Cleanup
    *Run code cleanup.

--- app/src/main/java/com/github/florent37/tuto/TutoShowcaseMainActivity.java
+++ app/src/main/java/com/github/florent37/tuto/TutoShowcaseMainActivity.java
@@ -1,7 +1,7 @@
 package com.github.florent37.tuto;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
