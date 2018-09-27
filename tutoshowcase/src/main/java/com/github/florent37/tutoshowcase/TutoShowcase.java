8c2f375a1eb7baf3a02b121b0f96fe9877aa0802
javaherisaber <29440700+javaherisaber@users.noreply.github.com>
2018-09-27 12:29:33 +0330

    Fix swipable hands position in RTL layout
    when using an RTL layout swipeRight hand steps out of screen on viewPager

--- tutoshowcase/src/main/java/com/github/florent37/tutoshowcase/TutoShowcase.java
+++ tutoshowcase/src/main/java/com/github/florent37/tutoshowcase/TutoShowcase.java
@@ -244,18 +244,28 @@ public final class TutoShowcase {
             hand.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
                 @Override
                 public boolean onPreDraw() {
+                    boolean isLayoutRTL = TextUtilsCompat.getLayoutDirectionFromLocale(Locale.getDefault()) == ViewCompat.LAYOUT_DIRECTION_RTL;
+					
                     int x = (int) (rect.centerX() - hand.getWidth() / 2f);
                     int y = (int) (rect.centerY() - hand.getHeight() / 2f);
 
                     ViewCompat.setTranslationY(hand, y);
-                    ViewCompat.setTranslationX(hand, x);
+					if (!left && isLayoutRTL) {
+						ViewCompat.setTranslationX(hand, -x);
+					} else {
+						ViewCompat.setTranslationX(hand, x);
+					}
 
                     if (settings.animated) {
                         float tX;
                         if (left) {
                             tX = rect.left;
                         } else {
-                            tX = rect.left + rect.width() * 0.7f;
+                            if (isLayoutRTL) {
+								tX = rect.left;
+							} else {
+								tX = rect.left + rect.width() * 0.7f;
+							}
                         }
                         ViewCompat.animate(hand).translationX(tX)
                                 .setStartDelay(settings.delay != null ? settings.delay : 500)
